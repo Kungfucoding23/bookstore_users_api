b0f1bd8feec32b501ca1143e66403b8869357f6f
Alejandro <alesantin012@hotmail.com>
2021-03-12 11:36:19 -0300

    handle error

--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,11 @@
 package services
+
+import (
+	"github.com/Kungfucoding23/bookstore_users_api/domain/users"
+	"github.com/Kungfucoding23/bookstore_users_api/utils/errors"
+)
+
+//CreateUser service
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+}
